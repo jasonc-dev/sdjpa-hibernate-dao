6565a2e79dba0c82f23c3f237300cc5961a0bcf1
jasonc-dev <jasoncohen07@gmail.com>
2022-05-07 12:41:33 +0100

    implement save new author

--- src/main/java/jason/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/jason/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -37,7 +37,13 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.persist(author);
+        em.flush(); // flush transactions to DB
+        em.getTransaction().commit();
+
+        return author;
     }
 
     @Override
