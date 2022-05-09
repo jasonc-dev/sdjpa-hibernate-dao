121862e6ba29718c818b1de0ebcf34c9bff4d7c3
jasonc-dev <jasoncohen07@gmail.com>
2022-05-09 08:56:52 +0100

    implement bookDAO + fix jdbc connection bug

--- src/main/java/jason/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/jason/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -21,18 +21,23 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author getById(Long id) {
-        return getEntityManager().find(Author.class, id);
+        EntityManager em = getEntityManager();
+        Author author = getEntityManager().find(Author.class, id);
+        em.close();
+        return author;
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-
+        EntityManager em = getEntityManager();
         TypedQuery<Author> query = getEntityManager().createQuery("SELECT a FROM Author a " +
                         "WHERE a.firstName = :first_name AND a.lastName = :last_name", Author.class);
         query.setParameter("first_name", firstName);
         query.setParameter("last_name", lastName);
 
-        return query.getSingleResult();
+        Author author = query.getSingleResult();
+        em.close();
+        return author;
     }
 
     @Override
@@ -42,6 +47,7 @@ public class AuthorDaoImpl implements AuthorDao {
         em.persist(author);
         em.flush(); // flush transactions to DB i.e. execute transactions to DB
         em.getTransaction().commit();
+        em.close();
 
         return author;
     }
@@ -49,12 +55,15 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public Author updateAuthor(Author author) {
         EntityManager em = getEntityManager();
-        em.joinTransaction();
-        em.merge(author);
-        em.flush();
-        em.clear();
-
-        return em.find(Author.class, author.getId());
+        try {
+            em.joinTransaction();
+            em.merge(author);
+            em.flush();
+            em.clear();
+            return em.find(Author.class, author.getId());
+        } finally {
+            em.close();
+        }
     }
 
     @Override
@@ -65,6 +74,7 @@ public class AuthorDaoImpl implements AuthorDao {
         em.remove(author);
         em.flush();
         em.getTransaction().commit();
+        em.close();
     }
 
     private EntityManager getEntityManager() {
